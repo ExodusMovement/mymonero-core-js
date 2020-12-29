a64381997424b57bbfa2aaaa60e9f30006cd1615
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2020-12-29 14:13:42 +0100

    fix: disable parallel mode for cmake

--- bin/build-emcpp.sh
+++ bin/build-emcpp.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-#EMCC_DEBUG=1 
+#EMCC_DEBUG=1
 
-mkdir -p build && 
-cd build && 
-emconfigure cmake .. && 
-emmake cmake --build . -j `nproc` && 
+mkdir -p build &&
+cd build &&
+emconfigure cmake .. &&
+emmake cmake --build . &&
 emmake make . -j `nproc`
