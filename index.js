022e673844e85bd0dc6effebe0a0d83046e5b8ef
Nikita Skovoroda <chalkerx@gmail.com>
2024-11-07 20:16:49 +0400

    feat!: drop bridge wrapper

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 'use strict'
 
-const MyMoneroCoreBridgeEssentialsClass = require('./monero_utils/MyMoneroCoreBridgeEssentialsClass.js')
 const MyMoneroCoreCppWASM = require('./monero_utils/MyMoneroCoreCpp_WASM.js')
 
 async function loadMyMoneroCore(options) {
-  const thisModule = await MyMoneroCoreCppWASM(options).ready
-  const instance = new MyMoneroCoreBridgeEssentialsClass(thisModule)
-  return instance
+  const Module = await MyMoneroCoreCppWASM(options).ready
+  return { Module }
 }
 
 module.exports = { loadMyMoneroCore }
